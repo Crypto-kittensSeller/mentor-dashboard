f531c0d185a70c738769ef0ef51946db4ae2af7f
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2019-02-24 18:36:08 +0300

    feat: deploy project mentor-dashboard

---
+++ precache-manifest.c02e2168c79a5a1473680b1914171d3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5357f7305c488abea30c",
+    "url": "/mentor-dashboard/static/js/runtime~main.5357f730.js"
+  },
+  {
+    "revision": "2412a73883981df737b1",
+    "url": "/mentor-dashboard/static/js/main.2412a738.chunk.js"
+  },
+  {
+    "revision": "15b5fbcfd3be050733bf",
+    "url": "/mentor-dashboard/static/js/2.15b5fbcf.chunk.js"
+  },
+  {
+    "revision": "2412a73883981df737b1",
+    "url": "/mentor-dashboard/static/css/main.619b8a55.chunk.css"
+  },
+  {
+    "revision": "31134bbad4ab89dad9116326d7a853b0",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
